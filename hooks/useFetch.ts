aaa29648bc0a1a90b6b9aa53e108ce0e18d999b9
Your Name <you@example.com>
2020-08-24 12:46:42 -0300

    type fix

--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -1,12 +1,12 @@
 import useSWR from 'swr';
 import api from '../services/api';
 
-export function useFetch<Data = any, Error = any>(url: string, interval?: number) {
+export function useFetch(url: string, interval?: number) {
     let int = 60000
     if (interval) {
         int = interval
     }
-    const { data, error } = useSWR<Data, Error>(
+    const { data, error } = useSWR(
         url,
         async url => {
             const response = await api.get(url)
