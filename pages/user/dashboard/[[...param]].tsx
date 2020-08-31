20638320c970fb9e1f624af6a70f8f17bbfe93e1
Your Name <you@example.com>
2020-08-31 19:53:19 -0300

    flex shrink 0

--- pages/user/dashboard/[[...param]].tsx
+++ pages/user/dashboard/[[...param]].tsx
@@ -73,6 +73,7 @@ const Dashboard: React.FC = () => {
           px={6}
           py={4}
           maxH="80px"
+          flexShrink={0}
         />
         <Flex flexGrow={1} justifyContent="flex-end" alignItems="flex-end">
           <Stack isInline mr={4} mb={4}>
