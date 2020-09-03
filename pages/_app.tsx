9eaada32d0bcb72188a8e9aad2dc19c5099bda39
Your Name <you@example.com>
2020-09-03 09:57:04 -0300

    disable stripe

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,9 @@ import '../styles/CardSectionStyles.css'
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 
-const stripePromise = loadStripe(
-  'pk_test_51HLxa6H1WKSbV7ewz55Q0G6SZ4dv1VIEyLTUJ6Jn7G7nrOEYNy6FwN02EeNiXLXqfgqTGVcAhORCywvPLa6No83b00Dvsf1xIT'
-)
+// const stripePromise = loadStripe(
+//   'pk_test_51HLxa6H1WKSbV7ewz55Q0G6SZ4dv1VIEyLTUJ6Jn7G7nrOEYNy6FwN02EeNiXLXqfgqTGVcAhORCywvPLa6No83b00Dvsf1xIT'
+// )
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -110,40 +110,38 @@ const Checkout: React.FC = () => {
   const handleCheckout = async () => {
     setLoading(true)
     if (formaPagamento == 'ST') {
-      if (!stripe || !elements) {
-        // Stripe.js has not yet loaded.
-        // Make sure to disable form submission until Stripe.js has loaded.
-        return
-      }
-
-      const result = await stripe.confirmCardPayment(
-        `${localStorage.getItem('clientSecret')}`,
-        {
-          payment_method: {
-            card: elements.getElement(CardElement),
-            billing_details: {
-              name: 'Jenny Rosen',
-            },
-          },
-        }
-      )
-
-      if (result.error) {
-        // Show error to your customer (e.g., insufficient funds)
-        console.log(result.error.message)
-      } else {
-        // The payment has been processed!
-        if (result.paymentIntent.status === 'succeeded') {
-          // Show a success message to your customer
-          // There's a risk of the customer closing the window before callback
-          // execution. Set up a webhook or plugin to listen for the
-          // payment_intent.succeeded event that handles any business critical
-          // post-payment actions.
-          const response = await api.post('checkout/', {
-            produtos: produtos,
-          })
-        }
-      }
+      // if (!stripe || !elements) {
+      //   // Stripe.js has not yet loaded.
+      //   // Make sure to disable form submission until Stripe.js has loaded.
+      //   return
+      // }
+      // const result = await stripe.confirmCardPayment(
+      //   `${localStorage.getItem('clientSecret')}`,
+      //   {
+      //     payment_method: {
+      //       card: elements.getElement(CardElement),
+      //       billing_details: {
+      //         name: 'Jenny Rosen',
+      //       },
+      //     },
+      //   }
+      // )
+      // if (result.error) {
+      //   // Show error to your customer (e.g., insufficient funds)
+      //   console.log(result.error.message)
+      // } else {
+      //   // The payment has been processed!
+      //   if (result.paymentIntent.status === 'succeeded') {
+      //     // Show a success message to your customer
+      //     // There's a risk of the customer closing the window before callback
+      //     // execution. Set up a webhook or plugin to listen for the
+      //     // payment_intent.succeeded event that handles any business critical
+      //     // post-payment actions.
+      //     const response = await api.post('checkout/', {
+      //       produtos: produtos,
+      //     })
+      //   }
+      // }
     } else if (formaPagamento == 'TR') {
       const data = new FormData()
       data.append('produtos', JSON.stringify(produtos))
