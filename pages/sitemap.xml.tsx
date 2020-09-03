da673407c8f6711d04753169e5c8ff6fa2b2a820
Your Name <you@example.com>
2020-09-03 01:26:14 -0300

    sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
-
-const SiteMap: React.FC = () => {
-    return
-        '
-        <?xml version="1.0" encoding="UTF-8"?>
-            <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-                <url>
-                 <loc>https://aaafuria.com.br</loc>
-    </url>
-    <url>
-        <loc>
-        https://aaafuria.com.br/loja
-        </loc>
-    </url>
-    <url>
-    <loc>
-    https://aaafuria.com.br/sejasocio
-    </loc>
-    </url>
-</urlset>
-'
+const SiteMap: any = () => {
+  return `
+      <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            <url>
+                <loc>https://aaafuria.com.br</loc>
+                <lastmod>2020-09-03</lastmod>
+                <changefreq>weekly</changefreq>
+            </url>
+            <url>
+                <loc>https://aaafuria.com.br/loja</loc>
+                <lastmod>2020-09-03</lastmod>
+                <changefreq>weekly</changefreq>
+                <priority>0.8</priority>
+            </url>
+            <url>
+                <loc>https://aaafuria.com.br/sejasocio</loc>
+                <lastmod>2020-09-03</lastmod>
+                <changefreq>monthly</changefreq>
+            </url>
+        </urlset>
+      `
 }
 
 export default SiteMap
