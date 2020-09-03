fa3190c861678c9ded51df336749433dd10ca990
Leonardo <leonunesbs@gmail.com>
2020-09-03 00:51:21 -0300

    sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,4 +1,8 @@
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+import React from 'react'
+
+export default const SiteMap: React.FC = () => {
+    return (
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>https://aaafuria.com.br</loc>
     </url>
@@ -13,3 +17,5 @@
     </loc>
     </url>
 </urlset>
+    )
+}
