761db714df902debc2bfbcd05024f1071cb3637b
Leonardo <leonunesbs@gmail.com>
2020-09-03 00:33:53 -0300

    viewport remove

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,10 +17,6 @@ export default function Home() {
           name="description"
           content="Página inicial da plataforma de Sócios @aaafuria"
         ></meta>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
       </Head>
       <Header />
       <Flex
