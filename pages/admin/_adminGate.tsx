efbc92a99a2443b948ad146bc54bc9ade8d76525
Your Name <you@example.com>
2020-09-04 21:01:37 -0300

    test

--- pages/admin/_adminGate.tsx
+++ pages/admin/_adminGate.tsx
@@ -6,19 +6,16 @@ import { Link } from '@chakra-ui/core'
 const AdminGate: React.FC = ({ children }) => {
   const router = useRouter()
   const [message, setMessage] = useState('Verificando permissões...')
-  useEffect(() => {
-    async function isStaff() {
-      const response = await api.get('core/api/is-staff/')
-      if (!response.ok) {
-        setMessage('Não autorizado')
-        return router.push('/')
-      } else {
-        return <>{children}</>
-      }
+  async function isStaff() {
+    const response = await api.get('core/api/is-staff/')
+    if (!response.ok) {
+      setMessage('Não autorizado')
+      return router.push('/')
+    } else {
+      return <>{children}</>
     }
-    isStaff()
-  })
-
+  }
+  isStaff()
   return <Link href="/">{message}</Link>
 }
 
