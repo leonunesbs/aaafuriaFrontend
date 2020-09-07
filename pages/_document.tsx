3c8b17fea8ba67a1efed12eee55130015c6d3196
Your Name <you@example.com>
2020-09-07 13:32:34 -0300

    viewport

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,10 @@ class MyDocument extends Document<DocumentProps> {
       <Html lang="pt-br">
         <Head>
           <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
 
           <meta
             name="google-site-verification"
