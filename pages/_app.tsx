93e3c94b4c6b26cbd01c5434dc2681e50b6103c0
Your Name <you@example.com>
2020-09-27 10:12:44 -0300

    gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,27 @@
 import ThemeContainer from '../contexts/Theme/ThemeContainer'
+import * as gtag from '../lib/gtag'
+
 import '../styles/CardSectionStyles.css'
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 // const stripePromise = loadStripe(
 //   'pk_test_51HLxa6H1WKSbV7ewz55Q0G6SZ4dv1VIEyLTUJ6Jn7G7nrOEYNy6FwN02EeNiXLXqfgqTGVcAhORCywvPLa6No83b00Dvsf1xIT'
 // )
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
     // <Elements stripe={stripePromise}>
     <ThemeContainer>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,12 +5,30 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 class MyDocument extends Document<DocumentProps> {
   render(): JSX.Element {
     return (
       <Html lang="pt-br">
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <meta charSet="utf-8" />
           <meta
             name="google-site-verification"
