7eab05566b09eb9b8383f5712b691bbc38b6d36c
Your Name <you@example.com>
2020-09-27 10:02:17 -0300

    GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,16 @@ class MyDocument extends Document<DocumentProps> {
     return (
       <Html lang="pt-br">
         <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','UA-179117508-1');`,
+            }}
+          />
+
           <meta charSet="utf-8" />
           <meta
             name="google-site-verification"
@@ -106,6 +116,11 @@ class MyDocument extends Document<DocumentProps> {
             href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
           />
         </Head>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=UA-179117508-1" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`,
+          }}
+        />
         <body>
           <Main />
 
